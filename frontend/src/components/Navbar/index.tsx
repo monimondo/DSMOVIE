d0f4fc462436b8c33fc2e2106dd3e730ee9ced24
monimondo <monicamondo87@gmail.com>
2022-03-12 00:22:57 -0300

    routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,22 +1,27 @@
-import {ReactComponent as GithubIcon} from '../../assets/img/github.svg';
+import { ReactComponent as GithubIcon } from '../../assets/img/github.svg'
 import './styles.css';
+
+
 function Navbar () {
-return (
+
+return(
 
     <header>
     <nav className="container">
         <div className="dsmovie-nav-content">
             <h1>DSMovie</h1>
-            <a href="https://github.com/monimondo" target="_blank" rel="noreferrer">
+            <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
                 <div className="dsmovie-contact-container">
                     <GithubIcon />
-                    <p className="dsmovie-contact-link">/MoniMondo</p>
+                    <p className="dsmovie-contact-link">/devsuperior</p>
                 </div>
             </a>
         </div>
     </nav>
 </header>
+
 );
+
 }
 
 export default Navbar;
---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,11 @@
+function Form() {
+
+    return (
+    
+        <h1>Página de Listagem</h1>
+    
+    )
+    
+    }
+    
+    export default Form;
